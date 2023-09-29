935b2e2db1b6951cb95827994d1b11928458a674
EugenioGutiérrez <eugenio.gutierrez@teameeng.com>
2023-09-29 16:27:23 +0200

    Set up dagger

--- app/src/main/java/com/eugenio/marvelsuperheroes/core/ui/MainActivity.kt
+++ app/src/main/java/com/eugenio/marvelsuperheroes/core/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.eugenio.marvelsuperheroes
+package com.eugenio.marvelsuperheroes.core.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.eugenio.marvelsuperheroes.core.theme.ui.MarvelSuperHeroesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
