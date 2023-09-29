376c2af4ff0edb152b21995072feb55915aa5040
EugenioGutiérrez <eugenio.gutierrez@teameeng.com>
2023-09-30 00:10:51 +0200

    Complete with parameters, and change search by characterId to nameStartsWith parameter

--- app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/data/network/MarvelAPI.kt
+++ app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/data/network/MarvelAPI.kt
@@ -2,7 +2,7 @@ package com.eugenio.marvelsuperheroes.superheroslist.data.network
 
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MarvelAPI {
 
@@ -10,11 +10,24 @@ interface MarvelAPI {
      * Get all characters from Marvel
      */
     @GET("v1/public/characters")
-    suspend fun getCharacters(): Response<CharactersResponse>
+    suspend fun getCharacters(
+        @Query("apikey") apiKey: String,
+        @Query("ts") timeStamp: Long,
+        @Query("hash") hash: String,
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    ): Response<CharactersResponse>
 
     /**
-     * Get specific character by [characterId]
+     * Get specific character by name
      */
-    @GET("v1/public/characters/{characterId}")
-    suspend fun getCharacterById(@Path("characterId") characterId: Int): Response<CharactersResponse>
+    @GET("v1/public/characters")
+    suspend fun getCharacterById(
+        @Query("nameStartsWith") name: String,
+        @Query("apikey") apiKey: String,
+        @Query("ts") timeStamp: Long,
+        @Query("hash") hash: String,
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    ): Response<CharactersResponse>
 }
