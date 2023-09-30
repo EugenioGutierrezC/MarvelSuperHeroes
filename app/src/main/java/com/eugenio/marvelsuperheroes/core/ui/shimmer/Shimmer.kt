707a44cfe554e5e354d6462e90ce98addce59d83
EugenioGutiérrez <eugenio.gutierrez@teameeng.com>
2023-09-30 19:17:13 +0200

    Added skeleton with shimmer

---
+++ app/src/main/java/com/eugenio/marvelsuperheroes/core/ui/shimmer/Shimmer.kt
@@ -0,0 +1,44 @@
+package com.eugenio.marvelsuperheroes.core.ui.shimmer
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
+
+val ShimmerColorShades = listOf(
+    Color.LightGray.copy(alpha = 0.9f),
+    Color.LightGray.copy(alpha = 0.2f),
+    Color.LightGray.copy(alpha = 0.9f)
+)
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    val transition = rememberInfiniteTransition(label = "")
+    val startOffsetX by transition.animateFloat(
+        initialValue = -2 * size.width.toFloat(),
+        targetValue = 2 * size.width.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000)
+        ), label = ""
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = ShimmerColorShades,
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    ).onGloballyPositioned {
+            size = it.size
+        }
+}
--- app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/ui/components/CharacterRow.kt
+++ app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/ui/components/CharacterRow.kt
@@ -1,24 +1,30 @@
 package com.eugenio.marvelsuperheroes.superheroslist.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.eugenio.marvelsuperheroes.R
 import com.eugenio.marvelsuperheroes.core.ui.CircularAsyncImage
 import com.eugenio.marvelsuperheroes.core.ui.multipreview.DarkAndLightPreview
+import com.eugenio.marvelsuperheroes.core.ui.shimmer.shimmerEffect
 import java.net.URL
 
 @Composable
@@ -66,6 +72,61 @@ fun CharacterRow(
     }
 }
 
+@Composable
+fun CharacterRowShimmer() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+                .shimmerEffect()
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+                    .height(40.dp)
+                    .padding(8.dp)
+                    .shimmerEffect()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .shimmerEffect()
+                )
+
+                Spacer(modifier = Modifier.width(4.dp))
+
+                Box(
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(16.dp)
+                        .shimmerEffect()
+                )
+            }
+        }
+    }
+}
+
 
 @Composable
 @DarkAndLightPreview
@@ -73,4 +134,10 @@ fun CharacterRowPreview() {
     val url =
         URL("https://i.annihil.us/u/prod/marvel/i/mg/9/50/4ce5a385a2e82/standard_fantastic.jpg")
     CharacterRow("Spiderman", "575", url)
+}
+
+@Composable
+@DarkAndLightPreview
+fun CharacterRowSkeletonPreview() {
+    CharacterRowShimmer()
 }
