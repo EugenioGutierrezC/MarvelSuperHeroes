950ac8b0ecf6b519c2bfc183395128460167144e
EugenioGutiérrez <eugenio.gutierrez@teameeng.com>
2023-09-30 18:16:11 +0200

    Refactor import naming

--- app/src/main/java/com/eugenio/marvelsuperheroes/core/ui/MainActivity.kt
+++ app/src/main/java/com/eugenio/marvelsuperheroes/core/ui/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.eugenio.marvelsuperheroes.core.theme.ui.MarvelSuperHeroesTheme
+import com.eugenio.marvelsuperheroes.core.ui.theme.MarvelSuperHeroesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
