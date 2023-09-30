d0923cd20bfaa45dcb95b1dd6b266f2c23e23d7e
EugenioGutiérrez <eugenio.gutierrez@teameeng.com>
2023-09-30 08:19:51 +0200

    Added again character by id

--- app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/data/network/MarvelAPI.kt
+++ app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/data/network/MarvelAPI.kt
@@ -2,6 +2,7 @@ package com.eugenio.marvelsuperheroes.superheroslist.data.network
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MarvelAPI {
@@ -21,8 +22,19 @@ interface MarvelAPI {
     /**
      * Get specific character by name
      */
-    @GET("v1/public/characters")
+    @GET("v1/public/characters/{characterId}")
     suspend fun getCharacterById(
+        @Path("characterId") characterId: Int,
+        @Query("apikey") apiKey: String,
+        @Query("ts") timeStamp: Long,
+        @Query("hash") hash: String,
+    ): Response<CharactersResponse>
+
+    /**
+     * Get specific character by name
+     */
+    @GET("v1/public/characters")
+    suspend fun getCharacterByName(
         @Query("nameStartsWith") name: String,
         @Query("apikey") apiKey: String,
         @Query("ts") timeStamp: Long,
