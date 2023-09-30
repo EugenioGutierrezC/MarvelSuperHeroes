0e85d84340774983e327476968f75e1dab691e3b
EugenioGutiérrez <eugenio.gutierrez@teameeng.com>
2023-09-30 19:17:32 +0200

    Created a list to show skeleton

--- app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/ui/CharactersListScreen.kt
+++ app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/ui/CharactersListScreen.kt
@@ -1,8 +1,35 @@
 package com.eugenio.marvelsuperheroes.superheroslist.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.eugenio.marvelsuperheroes.core.ui.multipreview.DarkAndLightPreview
+import com.eugenio.marvelsuperheroes.core.ui.theme.MarvelSuperHeroesTheme
+import com.eugenio.marvelsuperheroes.superheroslist.ui.components.CharacterRowShimmer
 
 @Composable
 fun CharactersListScreen() {
-    //Fill
+    LazyColumn {
+        items(10) {
+            CharacterRowShimmer()
+            Divider(Modifier.fillMaxSize())
+        }
+    }
+}
+
+@Composable
+@DarkAndLightPreview
+fun CharactersListScreenPreview() {
+    MarvelSuperHeroesTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CharactersListScreen()
+        }
+    }
 }
