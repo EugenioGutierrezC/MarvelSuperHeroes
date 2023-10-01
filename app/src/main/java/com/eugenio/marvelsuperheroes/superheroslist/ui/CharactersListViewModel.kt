68798ca27d4075bf886e687bfc7e005177b2ab39
EugenioGutiérrez <eugenio.gutierrez@teameeng.com>
2023-10-01 17:42:35 +0200

    Solve bug when back from detail reloaded again screen

--- app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/ui/CharactersListViewModel.kt
+++ app/src/main/java/com/eugenio/marvelsuperheroes/superheroslist/ui/CharactersListViewModel.kt
@@ -2,7 +2,9 @@ package com.eugenio.marvelsuperheroes.superheroslist.ui
 
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.eugenio.marvelsuperheroes.superheroslist.domain.CharactersUseCase
 import com.eugenio.marvelsuperheroes.superheroslist.ui.model.CharacterViewItem
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,5 +15,5 @@ import javax.inject.Inject
 class CharactersListViewModel @Inject constructor(
     private val charactersUseCase: CharactersUseCase
 ): ViewModel()  {
-    val charactersFlow: Flow<PagingData<CharacterViewItem>> = charactersUseCase.getCharactersPaged()
+    val charactersFlow: Flow<PagingData<CharacterViewItem>> = charactersUseCase.getCharactersPaged().cachedIn(viewModelScope)
 }
